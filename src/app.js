567a99a79e5bb9cb05fda60dd0a22b8f8d2362a8
Jess Chadwick <jesschadwick@gmail.com>
2015-10-06 22:36:43 -0400

    Adding form validation

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-angular.module("TodoApp", [])
+angular.module("TodoApp", ['ngAnimate'])
 
 	.run(function ($rootScope, $log, $window, $timeout) {
 
@@ -9,22 +9,40 @@ angular.module("TodoApp", [])
 			{ name: 'Dryclean cape', completed: true },
 			{ name: 'Save Gotham', completed: false },
 		];
-		
+
 		var filterQuery = { name: '', completed: '' };
 
-		function add(newTaskName) {
-			todos.push({ name: newTaskName, completed: false })
+		function add(newTask, form) {
+
+			if(form) {
+				if(!form.$dirty) {
+					$log.warn('Form is not dirty')
+					return false;
+				}	
+				if(!form.$valid) {
+					$log.warn('Form is not valid')
+					return false;
+				}	
+			}
+
+			todos.push({ name: newTask.name, completed: false });
+			
+			newTask.name = '';
+			
+			if(form) {
+				form.$setPristine();
+			}
 		}
-		
+
 		function remove(todo) {
 			todos.splice(todos.indexOf(todo), 1);
 		}
 
 		function clearCompleted() {
 			var completed = todos.filter(function (x) { return x.completed; })
-			
+
 			for (var i = 0; i < completed.length; i++) {
-				remove(completed[i]);				
+				remove(completed[i]);
 			}
 		}
 
@@ -33,8 +51,6 @@ angular.module("TodoApp", [])
 		$rootScope.filterQuery = filterQuery;
 		$rootScope.remove = remove;
 		$rootScope.todos = todos;
-	
+
 		$log.info('Application initialized');
-		
-		$timeout(function() { $window.alert('Ready!')}, 3000)
 	})
